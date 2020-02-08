e8047f3966690789e824d0b3232416adfac61427
Akash Bhave <akashbbhave@gmail.com>
2020-02-08 17:32:02 -0500

    Initialize Gatsby project with Tailwind CSS starter

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function NotFoundPage() {
+    return (
+        <Layout>
+            <SEO title="404: Not found" />
+            <div>
+                <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">Not found.</h2>
+            </div>
+        </Layout>
+    );
+}
+
+export default NotFoundPage;
