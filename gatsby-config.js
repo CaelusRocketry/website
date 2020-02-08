b67f6deab39bf2d28666ac6d6b3eebbbb1b1b040
Akash Bhave <akashbbhave@gmail.com>
2020-02-08 17:32:02 -0500

    Initialize Gatsby project with Tailwind CSS starter

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    siteMetadata: {
+        title: `Project Caelus`,
+        description: `Taking the world's first high school liquid-fueled rocket to the edge of space.`,
+        author: `Project Caelus`
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-tailwind`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#4dc0b5`,
+                display: `minimal-ui`,
+                icon: `src/images/tailwind-icon.png`
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-postcss',
+            options: {
+                postCssPlugins: [
+                    require(`tailwindcss`)(`./tailwind.config.js`),
+                    require(`autoprefixer`),
+                    require(`cssnano`)
+                ]
+            }
+        },
+        `gatsby-plugin-offline`
+    ]
+};
