93a47b21263f714dd63af19d056f4c5d1f6b67ba
Adit Pareek <aditepic10@gmail.com>
2020-02-08 19:26:56 -0500

    this is a test.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-    theme: {},
-    variants: {},
+    theme: {
+        fontFamily: {
+            sans: ['Karla', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+            "primary-dark": '#0D1321',
+            "secondary-dark": '#435058',
+            "primary-light": '#F3F7F0',
+            "secondary-light": '#C7CACB',
+            "accent": '#F72C25',
+
+        },
+    }
+    },
+    variants: {
+        opacity: ['responsive', 'hover']
+    },
     // https://github.com/tailwindcss/custom-forms
     plugins: [require('@tailwindcss/custom-forms')]
 };
