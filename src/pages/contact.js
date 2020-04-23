f283405a02eadd8ecd52d49ff090fe38a732f32f
Nurdin <nurdinhossain@gmail.com>
2020-04-23 19:30:44 -0400

    Akashify ContactPage component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,13 +4,14 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import exports from './../../tailwind.config';
 
-function ContactPage() {
-    return (
-        <Layout>
-            <SEO title="Contact Us!" />
-            <div>
-                <h2 className="bg-accent text-2xl font-bold inline-block my-3 p-3 text-primary-light">Contact us!</h2>
-                <p className="leading-loose my-3">Fill out your information and we will get back to you shortly.</p>
+const ContactPage = () => (
+    <Layout>
+        <SEO title="Contact Us!" />
+        <div>
+            <h2 className="bg-accent text-2xl font-bold inline-block my-3 p-3 text-primary-light">Contact us!</h2>
+            <p className="leading-loose my-3">Fill out your information and we will get back to you shortly.</p>
+
+            <form action="https://getform.io/f/1225c972-e2eb-4f91-b241-b0da2c03980f" method="POST">
                 <label class="block">
                     <span class="text-gray-700">Name</span>
                     <input type="name" class="form-input mt-1 my-5 block w-full" placeholder="John Smith"></input>
@@ -28,11 +29,10 @@ function ContactPage() {
                     <textarea class="form-textarea mt-1 my-5 block w-full" rows="3" placeholder="Say something..."></textarea>
                 </label>
                 <button className="bg-primary-light p-2">Submit</button>
-            </div>
-            
-        </Layout>
+            </form>
+        </div>
         
-    );
-}
+    </Layout>
+)
 
 export default ContactPage;
