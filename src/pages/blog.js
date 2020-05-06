ca7277e5e97e76df993c1a6c05de04369e8c88d0
Akash Bhave <akashbbhave@gmail.com>
2020-05-06 10:39:49 -0400

    Add all brand images to /static/brand/

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Post from '../components/post'
-
-import { css, jsx } from '@emotion/core'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Post from "../components/post";
 
 const BlogPage = () => (
-    <Layout>
+  <Layout>
+    <SEO title="Blog" />
 
-        <SEO title="Blog" />
+    <div style={{ textAlign: "center" }}>
+      <h2 className="bg-accent text-2xl font-bold inline-block my-8 p-3 text-primary-light">
+        Blog
+      </h2>
+    </div>
 
-        <div style={{ textAlign: 'center' }}>
-            <h2 className="bg-accent text-2xl font-bold inline-block my-8 p-3 text-primary-light">Blog</h2>
-        </div>
-
-        <Post header="Programming is awesome">
-            Yo, we've been doing some crazy stuff!
-        </Post>
-        
-    </Layout>
-)
+    <Post header="Programming is awesome">
+      Yo, we've been doing some crazy stuff!
+    </Post>
+  </Layout>
+);
 
 export default BlogPage;
