d044dbdbcf6a6a51a30f4f21c24580aeb815d0e8
Akash Bhave <akashbbhave@gmail.com>
2020-05-06 12:14:26 -0400

    Design minimal hero section on home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,42 @@
 module.exports = {
-    siteMetadata: {
-        title: `Project Caelus`,
-        description: `Taking the world's first high school liquid-fueled rocket to the edge of space.`,
-        author: `Project Caelus`
+  siteMetadata: {
+    title: `Project Caelus`,
+    description: `Taking the world's first high school liquid-fueled rocket to the edge of space.`,
+    author: `Project Caelus`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/images/`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-tailwind`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#4dc0b5`,
-                display: `minimal-ui`,
-                icon: `src/images/tailwind-icon.png`
-            }
-        },
-        {
-            resolve: 'gatsby-plugin-postcss',
-            options: {
-                postCssPlugins: [
-                    require(`tailwindcss`)(`./tailwind.config.js`),
-                    require(`autoprefixer`),
-                    require(`cssnano`)
-                ]
-            }
-        },
-        `gatsby-plugin-offline`
-    ]
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-tailwind`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#4dc0b5`,
+        display: `minimal-ui`,
+        icon: `src/images/tailwind-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(`./tailwind.config.js`),
+          require(`autoprefixer`),
+          require(`cssnano`),
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,23 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-import Header from './header';
+import Header from "./header";
 
 function Layout({ children }) {
-    return (
-        <div className="flex flex-col font-sans min-h-screen text-gray-900">
-            <Header />
+  return (
+    <div className="font-sans bg-primary-dark text-primary-light flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col">
+        <Header className="flex-shrink-0" />
+        <main id="main">{children}</main>
+      </div>
 
-            <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
-                {children}
-            </main>
-
-            <footer className="bg-blue-700">
-                <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
-                    <p className="text-white">
-                        Created by{` `}
-                        <a className="font-bold no-underline text-white" href="https://bryant.io">
-                            Taylor Bryant
-                        </a>
-                    </p>
-
-                    <p>
-                        <a
-                            className="font-bold no-underline text-white"
-                            href="https://github.com/taylorbryant/gatsby-starter-tailwind">
-                            GitHub
-                        </a>
-                    </p>
-                </nav>
-            </footer>
-        </div>
-    );
+      <div className="pin-b">{/* <Footer /> */}</div>
+    </div>
+  );
 }
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
