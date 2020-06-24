4264184786e4929342d88508c61032663d2da47d
Srikar Gouru <srikarg89@gmail.com>
2020-06-24 13:53:02 -0400

    Create blogPost schema and edit blog page css

--- packages/cms/schema.js
+++ packages/cms/schema.js
@@ -5,6 +5,7 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import post from "./schemas/post";
+import blogPost from "./schemas/blogPost";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +13,5 @@ export default createSchema({
   name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([post]),
+  types: schemaTypes.concat([post, blogPost]),
 });
