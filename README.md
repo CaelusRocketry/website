4df3914fa777b05e688d501439a1a71c600a79c7
Srikar Gouru <srikarg89@gmail.com>
2020-06-24 00:59:58 -0400

    Add material-ui to package.json

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Website
 
 Monorepo managed with Lerna.
+
+## Setup
+
+Our repo is a _Monorepo_: Both web and CMS packages in one repo
+The _Monorepo_ is managed with Lerna
+
+### Website Setup
+
+```
+cd packages/web
+npm i (if first time)
+gatsby develop
+```
+
+### Sanity Setup
+
+```
+cd packages/cms
+npm i (if first time)
+sanity login (if not logged in, use contact@projectcaelus email/pass)
+sanity start
+```
--- packages/web/src/pages/blog.js
+++ packages/web/src/pages/blog.js
@@ -6,31 +6,36 @@ import ImagePost from "../components/imagePost";
 import SubPost from "../components/subpost"
 import Img from "gatsby-image"
 
-const BlogPage = ({ data }) => (
-  <Layout>
-    <SEO title="Blog" />
+const BlogPage = ({ data }) => {
 
-    <div style={{ textAlign: "center" }}>
-      <h2 className="font-serif text-4xl text-white-800 text-center mb-10" /*"bg-accent text-2xl font-bold inline-block my-8 p-3 text-primary-light"*/>
-        -- Blog --
-      </h2>
-    </div>
+  
 
-    <ImagePost header={data.posts.nodes[2].title} desc={data.posts.nodes[1].description}>
-      <Img fluid={data.posts.nodes[2].image.asset.fluid} />
-    </ImagePost>
+  return (
+    <Layout>
+      <SEO title="Blog" />
 
-    <section class='flex'>
-      {[1, 2, 3, 4].map((index) => (
-        <div class='flex-auto'>
-          <SubPost header="Example">
-            <Img fixed={data.posts.nodes[index].image.asset.fixed}/>
-          </SubPost>
-        </div>
-      ))}
-    </section>
-  </Layout>
-);
+      <div style={{ textAlign: "center" }}>
+        <h2 className="font-serif text-4xl text-white-800 text-center mb-10" /*"bg-accent text-2xl font-bold inline-block my-8 p-3 text-primary-light"*/>
+          -- Blog --
+        </h2>
+      </div>
+
+      <ImagePost header={data.posts.nodes[2].title} desc={data.posts.nodes[1].description}>
+        <Img fluid={data.posts.nodes[2].image.asset.fluid} />
+      </ImagePost>
+
+      <section class='flex'>
+        {[1, 2, 3, 4].map((index) => (
+          <div class='flex-auto'>
+            <SubPost header="Example">
+              <Img fixed={data.posts.nodes[index].image.asset.fixed}/>
+            </SubPost>
+          </div>
+        ))}
+      </section>
+    </Layout>
+  );
+}
 
 export default BlogPage;
 
