1064ceb4fb84b95d6bf28ba231db1ee3bd0d9f24
Aarya Patel <aaryapatel100@gmail.com>
2020-07-05 17:05:23 -0400

    Make use of query and insert title as well as image

--- packages/web/src/pages/outreach.js
+++ packages/web/src/pages/outreach.js
@@ -8,43 +8,46 @@ function OutreachPage({ data }) {
   return (
     <Layout>
       <SEO title="Outreach" />
-      <section className="flex flex-col bg-primary-dark justify-center">
-        <h1
-          style={{
-            fontSize: "50px",
-            fontWeight: 300,
-            textAlign: "center",
-            padding: 10,
-            marginTop: 50,
-          }}
-        >
-          {/*data.events.nodes.title*/}WTF
-        </h1>
-        <section class="flex container max-w-5xl mx-auto">
-          <div
-            class="flex-auto container md:w-1/2 justify-center"
-            style={{ margin: 5 }}
+      {data.events.nodes.map((event) => (
+        <section className="flex flex-col bg-primary-dark justify-center">
+          <h1
+            style={{
+              fontSize: "50px",
+              fontWeight: 300,
+              textAlign: "center",
+              padding: 10,
+              marginTop: 50,
+            }}
           >
-            <p className="" style={{ textAlign: "left" }}>
-              {data.events.nodes[0].description}
-            </p>
-          </div>
-          <div
-            class="flex-auto container md:w-1/2 justify-center"
-            style={{ margin: 5 }}
-          >
-            {/* <BackgroundImage
-              fluid={data.events.nodes.image.asset.fluid}
-              className="md:mt-0 -mt-16"
-              style={{
-                height: "55vh",
-                width: "100%",
-                backgroundSize: "contain",
-              }}
-            /> */}
-          </div>
+            {event.title}
+          </h1>
+          <section class="flex container max-w-5xl mx-auto">
+            <div
+              class="flex-auto container md:w-1/2 justify-center"
+              style={{ margin: 5 }}
+            >
+              <p className="" style={{ textAlign: "left" }}>
+                {/* {event.description} */}
+                why tf is description not working
+              </p>
+            </div>
+            <div
+              class="flex-auto container md:w-1/2 justify-center"
+              style={{ margin: 5 }}
+            >
+              <BackgroundImage
+                fluid={event.image.asset.fluid}
+                className="md:mt-0 -mt-16"
+                style={{
+                  height: "55vh",
+                  width: "100%",
+                  backgroundSize: "contain",
+                }}
+              />
+            </div>
+          </section>
         </section>
-      </section>
+      ))}
     </Layout>
   );
 }
