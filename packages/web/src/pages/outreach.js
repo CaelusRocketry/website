9c3e54ae34ceefe2c71977be24ea275353bb298d
Aarya Patel <aaryapatel100@gmail.com>
2020-07-08 18:44:25 -0400

    Create event component and format outreach page accordingly

--- packages/web/src/pages/outreach.js
+++ packages/web/src/pages/outreach.js
@@ -1,54 +1,15 @@
 import React from "react";
-import BackgroundImg from "gatsby-background-image";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Footer from "../components/footer";
+import Event from "../components/event";
 import '../css/github-markdown.css';
-const BlockContent = require('@sanity/block-content-to-react')
 
 function OutreachPage({ data }) {
   return (
     <Layout>
       <SEO title="Outreach" />
-      {data.events.nodes.map((event) => (
-        <section className="flex flex-col bg-primary-dark justify-center">
-          <h1
-            style={{
-              fontSize: "50px",
-              fontWeight: 300,
-              textAlign: "center",
-              padding: 10,
-              marginTop: 50,
-            }}
-          >
-            {event.title}
-          </h1>
-          <section class="flex container max-w-5xl mx-auto">
-            <div
-              class="flex-auto container md:w-1/2 justify-center"
-              style={{ margin: 5 }}
-            >
-              <div className="markdown-body" style={{ textAlign: "left" }}>
-                <BlockContent blocks={event.description} />
-              </div>
-            </div>
-            <div
-              class="flex-auto container md:w-1/2 justify-center"
-              style={{ margin: 10 }}
-            >
-              <BackgroundImg
-                fluid={event.image.asset.fluid}
-                className="md:mt-0 -mt-16"
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  backgroundSize: "contain",
-                }}
-              />
-            </div>
-          </section>
-        </section>
-      ))}
+      <Event data={data.events.nodes}></Event>
       <Footer></Footer>
     </Layout>
   );
