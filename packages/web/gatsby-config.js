fea5311cddc38f4254b12de71795d4554fd17c47
Akash Bhave <akashbbhave@gmail.com>
2020-07-21 14:03:50 -0400

    Remove extra images, update favicon

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -4,15 +4,15 @@ module.exports = {
   siteMetadata: {
     title: `Project Caelus`,
     description: `Taking the world's first high school liquid-fueled rocket to the edge of space.`,
-    author: `Project Caelus`,
+    author: `Project Caelus`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/images/`,
-      },
+        path: `${__dirname}/src/images/`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -20,14 +20,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `Project Caelus`,
+        short_name: `Project Caelus`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#4dc0b5`,
+        background_color: `#0D1321`,
+        theme_color: `#C7CACB`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
-      },
+        icon: `src/images/icon.svg`
+      }
     },
     {
       resolve: "gatsby-plugin-postcss",
@@ -35,18 +35,18 @@ module.exports = {
         postCssPlugins: [
           require(`tailwindcss`)(`./tailwind.config.js`),
           require(`autoprefixer`),
-          require(`cssnano`),
-        ],
-      },
+          require(`cssnano`)
+        ]
+      }
     },
     {
       resolve: `gatsby-source-sanity`,
       options: {
         projectId: process.env.GATSBY_SANITY_ID,
         dataset: process.env.GATSBY_SANITY_DATASET,
-        token: process.env.SANITY_TOKEN,
-      },
+        token: process.env.SANITY_TOKEN
+      }
     },
-    `gatsby-plugin-offline`,
-  ],
+    `gatsby-plugin-offline`
+  ]
 };
